9309ddc1b05c833a3c012eaab9cf82d76c9957a2
LastComrade <konarklohat123456@gmail.com>
2019-11-03 02:20:02 +0530

    PU

--- Source_code_for_programs/17.Program_to_print_prime_numbers_from_1_to_100.c
+++ Source_code_for_programs/17.Program_to_print_prime_numbers_from_1_to_100.c
@@ -2,22 +2,23 @@
 int main()
 {
     int num, k=0, x;
-    printf("Enter the number: ");
-    scanf("%d", &num);
-    for (x=1; x<=num; x++)
+    for (num=1; num<=100; num++)
     {
-        if (num%x == 0)
+        for (x=1; x<=num; x++)
         {
-            k++;
+            if (num%x == 0)
+            {
+                k++;
+            }
         }
     }
     if (k == 2)
-    printf("Number is a Prime Number");
+    printf("%d\n", num);
     else 
     {
         if (k == 1)
         printf("Number is neither a prime nor a composite");
         else
         printf("Number is not a prime number");
-    }
+    }    
 }
